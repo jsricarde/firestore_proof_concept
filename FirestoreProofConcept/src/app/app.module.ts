2726b248e5c01a0b9d7adb8c1ac4dbf7f949cfb5
jsricarde <jhormaza@cafetosoftware.com>
2018-01-05 01:02:32 -0500

    FIRE-02: Organize the code.
    * Add docs to the files below.
    
    FIRE-02: Organize the code.

--- FirestoreProofConcept/src/app/app.module.ts
+++ FirestoreProofConcept/src/app/app.module.ts
@@ -6,16 +6,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AppComponent } from './app.component';
+/* Add to necessary AngularFire modules */
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+/* End to add to necessary AngularFire modules */
 import { UserService } from './users/user.service';
 import { BookService } from './booking/book.service';
 import { BookFormComponent } from './booking/book-form/book-form.component';
 import { BooklistComponent } from './booking/booklist/booklist.component';
-
+// import environment data(firebase keys).
 import { environment } from '../environments/environment';
 
 @NgModule({
--- FirestoreProofConcept/src/app/booking/book.service.ts
+++ FirestoreProofConcept/src/app/booking/book.service.ts
@@ -1,23 +1,37 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
-
-import * as firebase from 'firebase/app';
-
 import { Book } from './book';
 import { UserService } from '../users/user.service';
 import { Observable } from 'rxjs/Observable';
 import { switchMap } from 'rxjs/operators';
+
+import * as firebase from 'firebase/app';
+
 @Injectable()
 export class BookService {
-
+  /**
+   * @type {Observable<Book>} book
+   * @type {AngularFirestoreCollection<Book>} booksCollection
+   */
   book: Observable<Book | null>;
   booksCollection: AngularFirestoreCollection<Book>;
 
+  /**
+   * @constructor BookService
+   * @param {AngularFirestore} afs
+   * @param {UserService} userService
+   */
   constructor(private afs: AngularFirestore, private userService: UserService) {
     this.booksCollection = this.afs.collection('bookings');
   }
 
+  /**
+   * create void function
+   * Add a new book to bookings collection.
+   * @param {Book} bookData
+   * @memberof BookService
+   */
   create(bookData: Book) {
     // Persist a document id
     const id = this.afs.createId();
@@ -25,6 +39,13 @@ export class BookService {
     this.booksCollection.add(bookData);
   }
 
+  /**
+   * getByUserRef function
+   * Get bunch of books by userRef
+   * @param {any} userRef
+   * @returns {AngularFirestoreCollection<Book>} booksCollection
+   * @memberof BookService
+   */
   getByUserRef(userRef) {
     return this.booksCollection = this.afs.collection('bookings', ref => ref.where('user', '==', userRef));
   }
